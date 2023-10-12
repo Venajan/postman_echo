394af0267fffd5dd93741c809eede572a0e5bc53
Venajan <pokrovskiyalex77@gmail.com>
2023-10-12 16:55:17 +0300

    Add tests

---
+++ src/test/java/ru/netology/postmanEchoTest.java
@@ -0,0 +1,36 @@
+package ru.netology;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import org.junit.jupiter.api.Test;
+class postmanEchoTest {
+
+    @Test
+    void shouldTestSomeData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Добрый день")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hello world"))
+        ;
+    }
+
+
+    @Test
+    void shouldTestPost() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello world")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Добрый день"))
+
+        ;
+    }
+}
